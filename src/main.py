d640c3d1b40ea3f122ce1d04fcc49addce0fbd7c
LezardSC <117669219+LezardSC@users.noreply.github.com>
2025-07-03 18:58:10 +0200

    feat: normalize and denormalize datas, and show them in a graph

--- src/main.py
+++ src/main.py
@@ -1,22 +1,38 @@
 import os
+import numpy as np
 import pandas as pds
-from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-csv_path = os.path.join(script_dir, '../data/data.csv')
+def draw_data(x, y):
+	fig, ax = plt.subplots()
+	ax.set_xlabel('km')
+	ax.set_ylabel('price')
+	ax.set_title('linear regression')
+	ax.scatter(x, y, alpha=0.6)
+	plt.tight_layout()
+	plt.show()
 
 def parse_data():
+	script_dir = os.path.dirname(os.path.abspath(__file__))
+	csv_path = os.path.join(script_dir, '../data/data.csv')
 	data = pds.read_csv(csv_path)
 	
-	scaler = MinMaxScaler()
 	km = data[['km']].values
 	price = data[['price']].values
-	scaler.fit(price)
-	price_scaled = scaler.transform(price)
+	km_scaled = (km - km.min(axis=0)) / (km.max(axis=0) - km.min(axis=0))
+	price_scaled = (price - price.min(axis=0)) / (price.max(axis=0) - price.min(axis=0))
+	km_unscaled = km_scaled * (km.max(axis=0) - km.min(axis=0)) + km.min(axis=0)
+	price_unscaled = price_scaled * (price.max(axis=0) - price.min(axis=0)) + price.min(axis=0)
+	# print(km)
+	# print(price)
+	# print(f'my price scaled: {price_scaled}')
+	
+	# draw_data(km, price)
 
-	print(km)
-	print(price)
-	print(f'price scaled: {price_scaled}')
+# y = theta0 + (theta1 * x)
+
+def main():
+	parse_data()
 
 if __name__ == '__main__':
-	parse_data()
+	main()
